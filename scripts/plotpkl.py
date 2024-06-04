2d257c5e90cab73045a8d4af520c585545da7377
LarsonLaugh <lixian.philips.wang@gmail.com>
2024-06-04 18:55:31 +0200

    refactor plotpkl to allow batch plotting

--- scripts/plotpkl.py
+++ scripts/plotpkl.py
@@ -1,32 +1,77 @@
 import matplotlib.pyplot as plt
 import pickle
 import sys
+import os
+from xkwant.templates import *
+from xkwant.utils import density_to_energy
 
 if len(sys.argv) == 1:
-    filename = "data_dirachbar.py_20240529_1041.pkl"
+    directory_path = "data_dirachbar.py_20240529_1041.pkl"
 else:
-    filename = sys.argv[1]
+    directory_path = sys.argv[1]
 
-with open(filename, "rb") as f:
+with open(directory_path, "rb") as f:
     data = pickle.load(f)
 
-fig, axes = plt.subplots(3, 1, figsize=(8, 12), tight_layout=True)
+filename = os.path.basename(directory_path)
+fig, axes = plt.subplots(2, 2, figsize=(10, 12))
 fig.suptitle(
-    f"filename:{filename}\nIin:{data['Iin']}\nidos_kpm: {data['idos_kpm']}\nN1: {data['N1']},L: {data['L']}\nhamiltonian_params_sys: {data['hamiltonian_params_sys']}",
+    f"filename:{filename}\nIin:{data['Iin']}\nidos_kpm: {data['idos_kpm']}\nN1: {data['N1']},L: {data['L']:0.2f}\nhamiltonian_params_sys: {data['hamiltonian_params_sys']}",
     x=0.5,
-    y=0.98,
+    y=0.95,
     ha="center",
 )
-axes[0].plot(data["densities"], data["voltage_V12"], color="k")
-axes[0].scatter(data["densities"], data["voltage_V12"], s=15, color="r")
-axes[0].set_xlabel("Density [nm$^{-2}$]")
-axes[0].set_ylabel("V34 [$\\mu$V]")
-axes[1].plot(data["densities"], data["voltage_V34"], color="k")
-axes[1].scatter(data["densities"], data["voltage_V34"], s=15, color="r")
-axes[1].set_xlabel("Density [nm$^{-2}$]")
-axes[1].set_ylabel("V12 [$\\mu$V]")
-axes[2].plot(data["idos"], data["idos_energy_range"], color="k")
-axes[2].scatter(data["idos"], data["idos_energy_range"], s=15, color="r")
-axes[2].set_xlabel("Density [nm$^{-2}$]")
-axes[2].set_ylabel("Energy [eV]")
-plt.show()
+
+fig.subplots_adjust(top=0.85)
+
+geop = data["geometric_params"]
+hamp_sys = data["hamiltonian_params_sys"]
+hamp_lead = data["hamiltonian_params_lead"]
+idos = data["idos"]
+idos_energy_range = data["idos_energy_range"]
+densities = data["densities"]
+
+try:
+    syst = doubledirac_mkhbar_4t(geop, hamp_sys, hamp_lead)
+except KeyError:
+    try:
+        syst = gappeddirac_mkhbar_4t(geop, hamp_sys, hamp_lead)
+    except KeyError:
+        syst = mkhbar_4t(geop, hamp_sys, hamp_lead)
+
+
+max_eng, min_eng = density_to_energy(
+    idos, idos_energy_range, max(densities)
+), density_to_energy(idos, idos_energy_range, min(densities))
+
+kwant.plotter.bands(syst.finalized().leads[0], ax=axes[0][0])
+axes[0][0].axhline(y=max_eng, linestyle="--")
+axes[0][0].axhline(y=min_eng, linestyle="--")
+
+axes[0][1].plot(data["densities"], data["voltage_V12"], color="k")
+axes[0][1].scatter(data["densities"], data["voltage_V12"], s=15, color="r")
+axes[0][1].set_xlabel("Density [nm$^{-2}$]")
+axes[0][1].set_ylabel("V34 [$\\mu$V]")
+axes[1][1].plot(data["densities"], data["voltage_V34"], color="k")
+axes[1][1].scatter(data["densities"], data["voltage_V34"], s=15, color="r")
+axes[1][1].set_xlabel("Density [nm$^{-2}$]")
+axes[1][1].set_ylabel("V12 [$\\mu$V]")
+axes[1][0].plot(data["idos"], data["idos_energy_range"], color="k")
+axes[1][0].scatter(data["idos"], data["idos_energy_range"], s=15, color="r")
+axes[1][0].set_xlabel("Density [nm$^{-2}$]")
+axes[1][0].set_ylabel("Energy [eV]")
+
+
+# axes[0].plot(data["densities"], data["voltage_V12"], color="k")
+# axes[0].scatter(data["densities"], data["voltage_V12"], s=15, color="r")
+# axes[0].set_xlabel("Density [nm$^{-2}$]")
+# axes[0].set_ylabel("V34 [$\\mu$V]")
+# axes[1].plot(data["densities"], data["voltage_V34"], color="k")
+# axes[1].scatter(data["densities"], data["voltage_V34"], s=15, color="r")
+# axes[1].set_xlabel("Density [nm$^{-2}$]")
+# axes[1].set_ylabel("V12 [$\\mu$V]")
+# axes[2].plot(data["idos"], data["idos_energy_range"], color="k")
+# axes[2].scatter(data["idos"], data["idos_energy_range"], s=15, color="r")
+# axes[2].set_xlabel("Density [nm$^{-2}$]")
+# axes[2].set_ylabel("Energy [eV]")
+plt.savefig(f"plots//{filename}.png")
