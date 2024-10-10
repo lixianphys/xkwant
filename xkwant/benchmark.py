3c7cc6682542f6910b60f4cae5ea789436f0cdd4
LarsonLaugh <lixian.philips.wang@gmail.com>
2024-10-10 13:53:10 +0200

    replacing sys.argv with click in parsing augments passed via cli

--- xkwant/benchmark.py
+++ xkwant/benchmark.py
@@ -4,6 +4,9 @@ import psutil
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import click
+import ast
 
 from xkwant.batch import *
 from xkwant.templates import *
@@ -11,6 +14,7 @@ from xkwant.physics import *
 from xkwant.utils import *
 from xkwant.config import LATTICE_CONST_HGTE
 import xkwant.templates as mytemplates
+
 
 '''
 This benchmark is to access the performance and scalability of kwant model.
@@ -57,8 +61,22 @@ def benchmark_model(template, model_size=10):
 
     return num_sites, execution_time, memory_usage
 
+@click.command()
+@click.argument("template_name", required=False, default="mkhbar_4t")
+@click.argument("model_sizes", required=False, default="[10,10,20,50]")
+def benchmark(template_name,model_sizes):
+    model_sizes = ast.literal_eval(model_sizes)
+    try:
+        template = getattr(mytemplates, template_name)
+        click.echo(f"Benchmarking template: {template_name}")
 
-def run_benchmark(template,model_sizes):
+
+        # You can now use the `template` in your benchmarking logic
+    except AttributeError:
+        click.echo(f"Template {template_name} not found in the module xkwant.templates", err=True)
+        raise SystemExit(1)
+
+
     results = []
     for model_size in model_sizes:
         print(f"running model_size={model_size}")
@@ -71,34 +89,7 @@ def run_benchmark(template,model_sizes):
                 "memory_usage": memory_usage,
             }
         )
-    return results
 
-
-if __name__ == "__main__":
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import sys
-
-    # Modify model_sizes
-    model_sizes = [10, 10, 20, 50] # the first run of model_sizes == "10" will includes overhead time for starting the calculation, thus will be removed in statistic data. 
-
-
-    if len(sys.argv) == 1:
-        print("No additional argument is provided. Default model type: mkhbar_4t will be benchmarked!")
-        template = mkhbar_4t
-    elif len(sys.argv) == 2:
-        template_name = sys.argv[1]
-        try:
-            template = getattr(mytemplates,template_name) # Dynamically get the template from the module using getattr
-        except AttributeError:
-            print(f"template {template_name} not found in the module xkwant.templates")
-            sys.exit(1)
-    else:
-        raise AttributeError("too many arguments! template as the only argument is allowed")
-
-
-    results = run_benchmark(template,model_sizes)
-    # Convert results to a DataFrame
     df = pd.DataFrame(results)
     # Plot the results
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6))
@@ -117,3 +108,59 @@ if __name__ == "__main__":
     ax1.set_ylabel("Execution Time [Second]")
     ax2.set_ylabel("Execution Time [Second]")
     plt.show()
+
+if __name__ == "__main__":
+
+    benchmark()
+
+
+    # # Modify model_sizes
+    # model_sizes = [10, 10, 20, 50] # the first run of model_sizes == "10" will includes overhead time for starting the calculation, thus will be removed in statistic data. 
+
+
+    # @click.command()
+    # @click.argument("template_name", required=False, default="mkhbar_4t")
+    # def benchmark(template_name):
+    #     """Command-line tool to benchmark a specific model template."""
+    #     try:
+    #         template = getattr(mytemplates, template_name)
+    #         click.echo(f"Benchmarking template: {template_name}")
+    #         # You can now use the `template` in your benchmarking logic
+    #     except AttributeError:
+    #         click.echo(f"Template {template_name} not found in the module xkwant.templates", err=True)
+    #         raise SystemExit(1)
+
+    # if len(sys.argv) == 1:
+    #     print("No additional argument is provided. Default model type: mkhbar_4t will be benchmarked!")
+    #     template = mkhbar_4t
+    # elif len(sys.argv) == 2:
+    #     template_name = sys.argv[1]
+    #     try:
+    #         template = getattr(mytemplates,template_name) # Dynamically get the template from the module using getattr
+    #     except AttributeError:
+    #         print(f"template {template_name} not found in the module xkwant.templates")
+    #         sys.exit(1)
+    # else:
+    #     raise AttributeError("too many arguments! template as the only argument is allowed")
+
+
+    # results = run_benchmark(template,model_sizes)
+    # # Convert results to a DataFrame
+    # df = pd.DataFrame(results)
+    # # Plot the results
+    # fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6))
+    # ax1.scatter(
+    #     df["model_size"].tolist()[1:],
+    #     df["execution_time"].tolist()[1:],
+    #     label=f"Execution Time ",
+    # )
+    # ax2.scatter(
+    #     df["num_sites"].tolist()[1:],
+    #     df["execution_time"].tolist()[1:],
+    #     label=f"Memory Usage",
+    # )
+    # ax1.set_xlabel("Model Size in 1D")
+    # ax2.set_xlabel("Number of Sites (2D)")
+    # ax1.set_ylabel("Execution Time [Second]")
+    # ax2.set_ylabel("Execution Time [Second]")
+    # plt.show()
